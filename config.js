aadc9987ccee0557548c3405fde5b5490a37c7ab
Nuno Amaral <n20amaral@outlook.com>
2018-10-26 17:02:05 +0000

    initial commit

---
+++ config.js
@@ -0,0 +1,22 @@
+			// More info about config & dependencies:
+			// - https://github.com/hakimel/reveal.js#configuration
+			// - https://github.com/hakimel/reveal.js#dependencies
+			Reveal.initialize({
+				controls: true,
+				keyboard: true,
+				multiplex: {
+					// Example values. To generate your own, see the socket.io server instructions.
+					secret: '15378288489473455542', // null so the clients do not have control of the master presentation
+					id: 'da0eae246b511a0d', // id, obtained from socket.io server
+					url: 'https://reveal-js-multiplex-ccjbegmaii.now.sh' // Location of socket.io server
+				},
+				dependencies: [
+
+					{ src: 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.3.5/socket.io.min.js', async: true },
+					{ src: 'plugin/multiplex/master.js', async: true },
+					{ src: 'plugin/markdown/marked.js' },
+					{ src: 'plugin/markdown/markdown.js' },
+					//{ src: 'plugin/notes/notes.js', async: true },
+					{ src: 'plugin/highlight/highlight.js', async: true, callback: function () { hljs.initHighlightingOnLoad(); } }
+				]
+			});
